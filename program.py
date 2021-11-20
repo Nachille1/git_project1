f3442edc471d8787714a1d2abfa202355483459d
Nikita <nikitka_smirnov_2007@list.ru>
2021-11-20 16:46:28 +0300

    Third commit

--- program.py
+++ program.py
@@ -1,8 +1,10 @@
 def main():
     print('First GIT program')
     print('And I change it every day')
+    print('Again')
 
 
 if __name__ == '__main__':
     main()
 # 48f4169618bdf7b92a75f3e2f164bc0d3b5d370e
+# https://github.com/Nachille1/git_project1.git
